49e6c3a51a9c043e69308f5e9f3cc80adce6f30c
Courtney Solari <csolari@ufl.edu>
2019-10-17 11:55:32 -0500

    Add stylesheet and JS files through functions.php

---
+++ functions.php
@@ -0,0 +1,24 @@
+<?php
+
+	/*==============================================
+
+		Add stylesheet and JS files
+
+	===============================================*/
+
+	function custom_theme_scripts() {
+
+		// Bootstrap Integration
+		wp_enqueue_style("bootstrap", get_template_directory_uri() . "/css/bootstrap.min.css");
+
+		// Main CSS
+		wp_enqueue_style("main-styles", get_stylesheet_uri());
+
+		// JavaScript Files
+		wp_enqueue_script("custom-js", get_template_directory_uri() . "/js/main.js");
+		wp_enqueue_script("bootstrap-js", get_template_directory_uri() . "/js/bootstrap.min.js");
+	}
+
+	add_action("wp_enqueue_scripts", "custom_theme_scripts");
+
+?>
