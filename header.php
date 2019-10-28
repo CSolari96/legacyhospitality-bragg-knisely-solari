584203f22b8d0e364e9fed3d0f6339c33cf249f5
Valerie Bragg <vbragg@ufledu>
2019-10-27 22:17:26 -0400

    Edit header code

--- header.php
+++ header.php
@@ -18,10 +18,10 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-lg-3">
-					<?php if(get_header_image()== ''){ ?>
+					<?php if(get_header_image() == ''){ ?>
 						<h1><a href="<?php get_home_url(); ?>"><?php bloginfo('name'); ?></a></h1><?php
-					}else {?>
-						<a href="<?php get_home_url(); ?>"><img src="<?php header_image(); ?>" height="<?php echo get_customer_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="Logo" /></a>
+					}else{?>
+						<a href="<?php get_home_url();?>"><img src="<?php header_image(); ?>" height="<?php echo get_customer_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="Logo" /></a>
 					<?php }?>
 				</div>
 
