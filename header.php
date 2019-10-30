0622b9e6810a73c4650c3361ef085f29f40bf350
Valerie Bragg <vbragg@ufledu>
2019-10-29 21:31:27 -0400

    Change double quotes to single quotes in functions.php

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-	<title><?php bloginfo("name"); ?></title>
+	<title><?php bloginfo('name'); ?></title>
 
 	<?php wp_head(); ?>
 
