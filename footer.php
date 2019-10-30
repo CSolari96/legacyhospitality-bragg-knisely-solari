bd7cbf793398e52b66cb16ddbf64e1dd7dc1a60f
Valerie Bragg <vbragg@ufledu>
2019-10-29 23:34:47 -0400

    Center items in footer

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer>
   <div class="row">
-    <div class="col-lg-4 d-flex align-items-center">
+    <div class="col-lg-4 d-flex align-items-center justify-content-center">
       <nav>
         <?php
           if(has_nav_menu('footer-left')){
@@ -12,11 +12,11 @@
       </nav>
   </div>
 
-  <div class="col-lg-4 d-flex align-items-center">
+  <div class="col-lg-4 d-flex align-items-center justify-content-center">
     <?php dynamic_sidebar('middle-footer') ?>
   </div>
 
-  <div class="col-lg-4 d-flex align-items-center">
+  <div class="col-lg-4 d-flex align-items-center justify-content-center">
     <?php dynamic_sidebar('right-footer') ?>
   </div>
 </div>
