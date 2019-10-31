70c11d106c5459837b4ff68029a408a3049750bb
Valerie Bragg <vbragg@ufledu>
2019-10-30 22:05:57 -0400

    Add content to 404 page

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+<div class="container">
+  <div class="row">
+    <main class="col-md-12">
+      <h2>We truly apoligize...</h2>
+      <p>We appreciate our website visitors, but that link no longer exists. Please head back to our <a href="<?php echo home_url('/');?>">homepage</a> to learn more about Legacy Hospitality.</p>
+      <img src="<?php get_template_directory_uri() . '/img/logo_placeholder.jpg' ?>" alt= "Error message" />
+    </main>
+
+  </div>
+</div>
