865b44341ee95e583ff8edf01b34bda388a5df95
Valerie Bragg <vbragg@ufledu>
2019-11-01 21:39:02 -0400

    Add test content to index page

--- index.php
+++ index.php
@@ -1,5 +1,27 @@
 <?php get_header(); ?>
+<div class="container">
+  <div class="row">
+    <main>
+      <?php
+        if(have_posts()){
+          while(have_posts()){
+            the_post(); ?>
+            <div class="col-md-12">
 
+              <h2 class="entry-title"><?php the_title(); ?></h2>
+              <p class="post-info"><?php echo "Published " . get_the_date(); ?></p>
+              <?php the_excert(); ?>
+
+              <a class="btn btn-secondary btn-sm" href="<?php the_permalink(); ?>">Read more</a>
+            </div>
+            <?php the_content(); ?>
+        <?php  }//ends while loop
+        }//ends the if statement
+       ?>
+
+    </main>
+  </div>
+</div>
 
 
 <?php get_footer(); ?>
