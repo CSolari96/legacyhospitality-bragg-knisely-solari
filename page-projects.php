c77520d71636d9e65bfeeaf1b66f7c8f12f336c9
Valerie Bragg <vbragg@ufledu>
2019-11-01 21:34:06 -0400

    Add excert and button to posts

--- page-projects.php
+++ page-projects.php
@@ -7,9 +7,13 @@
             while(have_posts()){
               the_post(); ?>
               <div class="col-md-12">
-              </div>
-              <h2 class="entry-title"><?php the_title(); ?></h2>
 
+                <h2 class="entry-title"><?php the_title(); ?></h2>
+                <p class="post-info"><?php echo "Published " . get_the_date(); ?></p>
+                <?php the_excert(); ?>
+
+                <a class="btn btn-secondary btn-sm" href="<?php the_permalink(); ?>">Read more</a>
+              </div>
               <?php the_content(); ?>
           <?php  }//ends while loop
           }//ends the if statement
