521d15e6fb3fa131eeb7a345750c9acb911a86a9
Courtney Solari <csolari@ufl.edu>
2019-11-06 15:18:52 -0600

    Fix issue with variables not being defined

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
-// Mobile Hamburger Menu
-var menuClose 	= 	document.getElementById("close-menu"); 				// Store mobile menu close button
-var menuOpen 	= 	document.getElementById("open-mobile-menu");		// Store mobile menu open button
-var mobileMenu 	= 	document.getElementByClassName("mobile-menu")[0];	// Store mobile menu
+document.addEventListener("DOMContentLoaded", function(event) {
+	// Mobile Hamburger Menu
+	var menuClose 	= 	document.getElementById("close-menu"); 				// Store mobile menu close button
+	var menuOpen 	= 	document.getElementById("open-mobile-menu");		// Store mobile menu open button
+	var mobileMenu 	= 	document.getElementsByClassName("mobile-menu")[0];	// Store mobile menu
 
-// When the user clickes the mobile menu open button, display the mobile menu and hide the menu open button
-menuOpen.addEventListener("click", function() {
-	mobileMenu.style.width = 100%;
-	menuOpen.classList.add("hide");
-});
+	// When the user clickes the mobile menu open button, display the mobile menu and hide the menu open button
+	menuOpen.addEventListener("click", function() {
+		mobileMenu.style.width = "100%";
+		menuOpen.classList.add("hide");
+	});
 
-// When the user clicks the mobile menu close button, hide the mobile menu and display the menu open button
-menuClose.addEventListener("click", function () {
-	mobileMenu.style.width = "0";
-	menuOpen.classList.remove("hide");
+	// When the user clicks the mobile menu close button, hide the mobile menu and display the menu open button
+	menuClose.addEventListener("click", function () {
+		mobileMenu.style.width = "0";
+		menuOpen.classList.remove("hide");
+	});
 });
