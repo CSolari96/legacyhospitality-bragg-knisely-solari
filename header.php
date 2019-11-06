b2784a431e3b45c981cd48732a4f4d1d821cde58
Courtney Solari <csolari@ufl.edu>
2019-11-06 14:59:40 -0600

    Add mobile navigation to home page

--- header.php
+++ header.php
@@ -15,27 +15,64 @@
 <body>
 
 	<header>
-		<div class="container">
-			<div class="row">
-				<div class="col-md-3">
-					<?php if(get_header_image() == ''){?>
-						<h1><a href="<?php echo get_home_url();?>"><?php bloginfo('name'); ?></a></h1><?php
-					}else{?>
-						<a href="<?php echo get_home_url();?>"><img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="Logo" /></a>
-					<?php } ?>
-				</div>
 
-			<div class="col-md-9" id="top-menu">
-				<nav>
-					<?php
-						if(has_nav_menu('top-menu')){
-						wp_nav_menu(array('theme_location' => 'top-menu' , 'container_class' => 'top-menu-class'));
-					}else{
+		<div class="container">
+
+			<nav class="mobile-menu">
+
+				<p><a href="javascript:void()" id="close-menu">&times;</a></p>
+
+				<?php
+					if (has_nav_menu('top-menu')) {
+
+						wp_nav_menu(array('theme_location' => 'top-menu' , 'container_class' => 'mobile-top-menu-class'));
+					
+					} else {
+
 						echo "Please select a top menu through the dashboard";
 					}
-					?>
-				</nav>
+				?>
+
+			</nav>
+
+			<div class="row">
+
+				<div class="col-md-3">
+
+					<?php 
+						if (get_header_image() == '') { ?>
+
+							<h1><a href="<?php echo get_home_url();?>"><?php bloginfo('name'); ?></a></h1>
+							
+				  	<?php } else { ?>
+							
+							<a href="<?php echo get_home_url();?>"><img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="Logo" /></a>
+					
+					<?php } ?>
+
+				</div>
+
+				<div class="col-md-9" id="top-menu">
+
+					<nav class="desktop-nav">
+
+						<?php
+							if (has_nav_menu('top-menu')) {
+
+								wp_nav_menu(array('theme_location' => 'top-menu' , 'container_class' => 'top-menu-class'));
+
+							} else {
+
+								echo "Please select a top menu through the dashboard";
+
+							}
+						?>
+					</nav>
+
+				</div>
+
+			</div>
+
 		</div>
-	</div>
 
 	</header>
