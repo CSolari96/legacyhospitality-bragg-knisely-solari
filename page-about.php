799b805e054e9694fc42f70213fc7310a8180ec5
Valerie Bragg <vbragg@ufledu>
2019-11-06 17:29:19 -0500

    Add left border to about

--- page-about.php
+++ page-about.php
@@ -9,9 +9,10 @@ Template Post Type: page
 <?php get_header(); ?>
   <div class="container">
     <main>
-      <div class="row">
-        <h1 class= "knowledge-power-title"><span id="h1-monserret">KNOWLEDGE <span class="red">+</span><br /> PASSION <span class="red">=</span></span><br /><span id="h1-gotham">POWER</span></h1>
-      </div>
+      <section class="red-left-border">
+        <div class="row">
+          <h1 class= "knowledge-power-title"><span id="h1-monserret">KNOWLEDGE <span class="red">+</span><br /> PASSION <span class="red">=</span></span><br /><span id="h1-gotham">POWER</span></h1>
+        </div>
 
       <div class="row">
         <div class="col-md-5">
@@ -22,6 +23,7 @@ Template Post Type: page
           <?php dynamic_sidebar('hero-image'); ?>
         </div>
       </div>
+    </section>
 
       <div class="row">
 
