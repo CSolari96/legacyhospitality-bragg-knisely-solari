b86e7b4741cda5fd1b525bc39ca41a10f823f25f
Valerie Bragg <vbragg@ufledu>
2019-11-06 17:50:21 -0500

    Add padding to buttons on projects page

--- page-projects.php
+++ page-projects.php
@@ -12,8 +12,10 @@
                 <p class="post-info"><?php echo "Published " . get_the_date(); ?></p>
 
               <?php the_content(); ?>
-              <a class="btn btn-secondary btn-sm project-buttons" href="<?php the_permalink(); ?>">Read more</a>
+              <div class ="project-buttons">
+                <a class="btn btn-secondary btn-sm" href="<?php the_permalink(); ?>">Read more</a>
               </div>
+            </div>
           <?php  }//ends while loop
           }//ends the if statement
          ?>
