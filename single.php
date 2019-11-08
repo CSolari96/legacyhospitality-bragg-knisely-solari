5e38b0ba7c563c418d83ecdfb741f5460837c494
Kenney Knisely <kknisely@mac.com>
2019-11-08 14:32:38 -0500

    Begin styling posts

--- single.php
+++ single.php
@@ -1,19 +1,33 @@
 <?php get_header(); ?>
-  <div class="container">
-    <div class="row">
-      <main class="col-md-12">
-        <?php
-          if(have_posts()){
-            while(have_posts()){
-              the_post(); ?>
 
-              <div class="single-post-featured-image">
-                <?php the_post_thumbnail('large') ?>
-              </div>
+  <?php
+    if(have_posts()){
+      while(have_posts()){
+        the_post(); ?>
 
-              <h2 class="entry-title"><?php the_title(); ?></h2>
+<!--Get the URL of the featured image to use as jumbotron background -->
+      <?php
+      $thumb_id = get_post_thumbnail_id();
+      $thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true);
+      $thumb_url = $thumb_url_array[0];
+      ?>
 
-              <p class="post-information"><?php echo "<span class='bold'>Published: </span>" . get_the_date(); echo " | <span class= 'bold'>Written by: </span>" . get_the_author(); ?></p>
+<!-- Jumbotron header -->
+      <div class="jumbotron jumbotron-fluid" style="background-image: url(<?php echo $thumb_url; ?>)">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <h1> <?php the_title(); ?> </h1>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+<!--Content-->
+    <div class="container">
+      <div class="row">
+        <main class="col-md-12">
 
               <?php the_content(); ?>
           <?php  }//ends while loop
