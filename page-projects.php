580bac40435a92af828d6a8bab90c261b2d5839c
Kenney Knisely <kknisely@mac.com>
2019-11-08 21:14:11 -0500

    Begin to create and style projects page

--- page-projects.php
+++ page-projects.php
@@ -1,25 +1,48 @@
+<?php
+
+/*
+Template Name: Projects Page Layout
+Template Post Type: page
+*/
+ ?>
+
 <?php get_header(); ?>
-  <div class="container">
-    <div class="row">
-      <main>
-        <?php
-          if(have_posts()){
-            while(have_posts()){
-              the_post(); ?>
-              <div class="col-md-12">
 
-                <h2 class="entry-title"><?php the_title(); ?></h2>
-                <p class="post-info"><?php echo "Published " . get_the_date(); ?></p>
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
 
-              <?php the_content(); ?>
-                <a class="btn btn-secondary btn-sm project-buttons" href="<?php the_permalink(); ?>">Read more</a>
+<!-- Code used to display all posts on one screen -->
+<?php
+// the query
+$wpb_all_query = new WP_Query(array('post_type'=>'post', 'post_status'=>'publish', 'posts_per_page'=>-1)); ?>
+
+<?php if ( $wpb_all_query->have_posts() ) : ?>
+
+
+    <!-- the loop -->
+    <?php while ( $wpb_all_query->have_posts() ) : $wpb_all_query->the_post(); ?>
+
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12 project-container">
+              <?php the_post_thumbnail("large");?>
+              <h1 class="project-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
             </div>
-          <?php  }//ends while loop
-          }//ends the if statement
-         ?>
+          </div>
+        </div>
+      </div>
 
-      </main>
-    </div>
-  </div>
+    <?php endwhile; ?>
+    <!-- end of the loop -->
+    <?php wp_reset_postdata(); ?>
 
+<?php else : ?>
+    <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+<?php endif; ?>
+
+
+</div>
+</div>
+</div>
 <?php get_footer(); ?>
