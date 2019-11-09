df5678c9849f951d1188043582f7bf7e09ef8f99
Courtney Solari <csolari@ufl.edu>
2019-11-09 15:45:15 -0600

    Replace about widget with content via WP loop

--- page-about.php
+++ page-about.php
@@ -21,7 +21,15 @@ Template Post Type: page
         
       <div class="container">
       
-        <?php dynamic_sidebar('about-legacy'); ?>
+        <?php
+          if(have_posts()){
+            while(have_posts()){
+              the_post(); ?>
+
+              <?php the_content(); ?>
+          <?php  }//ends while loop
+          }//ends the if statement
+         ?>
       
       </div>
           
