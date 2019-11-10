3204d6cfe9c1df4e3177f3a0b9a648fc53ae3fc6
Courtney Solari <csolari@ufl.edu>
2019-11-10 15:00:32 -0600

    Adjust column layout of 404 page

--- 404.php
+++ 404.php
@@ -1,11 +1,17 @@
 <?php get_header(); ?>
-<div class="container">
+<main class="container cannot-find-page">
   <div class="row">
-    <main class="col-md-12 cannot-find-page">
-      <h2 class= "bold">We truly apoligize...</h2>
-      <p>We appreciate your business, but that link no longer exists. Please head back to our <a href="<?php echo home_url('/');?>">homepage</a> to learn more about Legacy Hospitality.</p>
+    <div class="col-md-12">
+      <h2 class="bold">We truly apoligize...</h2>
+    </div>
+
+    <div class="col-md-6">
+	  <p>We appreciate your business, but that link no longer exists. Please head back to our <a href="<?php echo home_url('/');?>">homepage</a> to learn more about Legacy Hospitality.</p>
+    </div>
+
+    <div class="col-md-6">
       <img src="<?php echo get_template_directory_uri() . '/img/sorry-pencil.jpg'?>" alt="Error message" />
-    </main>
+    </div>
   </div>
-</div>
+</main>
   <?php get_footer(); ?>
