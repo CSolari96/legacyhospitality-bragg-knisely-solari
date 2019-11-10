0cb33ef590c804cc5a0deb5fbea7a81ac51bc360
Courtney Solari <csolari@ufl.edu>
2019-11-10 15:11:52 -0600

    Rearrange heading tag on 404 page

--- 404.php
+++ 404.php
@@ -1,11 +1,9 @@
 <?php get_header(); ?>
 <main class="container cannot-find-page">
   <div class="row">
-    <div class="col-md-12">
-      <h2 class="bold">We truly apoligize...</h2>
-    </div>
 
     <div class="col-md-6">
+      <h2 class="bold">We truly apoligize...</h2>
 	  <p>We appreciate your business, but that link no longer exists. Please head back to our <a href="<?php echo home_url('/');?>">homepage</a> to learn more about Legacy Hospitality.</p>
     </div>
 
