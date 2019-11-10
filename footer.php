53bb0f24f460a8d78fee7e70927f32dc1fd09ba5
Courtney Solari <csolari@ufl.edu>
2019-11-10 14:27:06 -0600

    Add container to footer

--- footer.php
+++ footer.php
@@ -1,23 +1,25 @@
 <footer>
-  <div class="row">
-    <div class="col-md-4 left-footer-section">
-      <nav>
-        <?php
-          if(has_nav_menu('footer-left')){
-          wp_nav_menu(array('theme_location' => 'footer-left' , 'container_class' => 'footer-left-class'));
-        }else{
-          echo "Please select a footer left menu through the dashboard";
-        }
-        ?>
-      </nav>
-  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4 left-footer-section">
+        <nav>
+          <?php
+            if(has_nav_menu('footer-left')){
+            wp_nav_menu(array('theme_location' => 'footer-left' , 'container_class' => 'footer-left-class'));
+          }else{
+            echo "Please select a footer left menu through the dashboard";
+          }
+          ?>
+        </nav>
+    </div>
 
-  <div class="col-md-4 middle-footer-section">
-    <?php dynamic_sidebar('middle-footer') ?>
-  </div>
+    <div class="col-md-4 middle-footer-section">
+      <?php dynamic_sidebar('middle-footer') ?>
+    </div>
 
-  <div class="col-md-4 right-footer-section">
-    <?php dynamic_sidebar('right-footer') ?>
+    <div class="col-md-4 right-footer-section">
+      <?php dynamic_sidebar('right-footer') ?>
+    </div>
   </div>
 </div>
 </footer>
