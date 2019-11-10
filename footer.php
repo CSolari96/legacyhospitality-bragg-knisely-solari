610cce1d30d53731c1337dae267669ee33b9d00c
Courtney Solari <csolari@ufl.edu>
2019-11-10 14:25:56 -0600

    Adjust classes on footer divs

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer>
   <div class="row">
-    <div class="col-lg-4 d-flex align-items-center justify-content-center">
+    <div class="col-md-4 left-footer-section">
       <nav>
         <?php
           if(has_nav_menu('footer-left')){
@@ -12,11 +12,11 @@
       </nav>
   </div>
 
-  <div class="col-lg-4 d-flex align-items-center justify-content-center">
+  <div class="col-md-4 middle-footer-section">
     <?php dynamic_sidebar('middle-footer') ?>
   </div>
 
-  <div class="col-lg-4 d-flex align-items-center justify-content-center">
+  <div class="col-md-4 right-footer-section">
     <?php dynamic_sidebar('right-footer') ?>
   </div>
 </div>
