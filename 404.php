8d17741b7728ba674995bac26b40838537858073
Courtney Solari <csolari@ufl.edu>
2019-11-10 15:31:25 -0600

    Add new 404 page image

--- 404.php
+++ 404.php
@@ -1,14 +1,13 @@
 <?php get_header(); ?>
 <main class="container cannot-find-page">
   <div class="row">
-
-    <div class="col-md-6">
-      <h2 class="bold">We truly <span class="red">apologize...</span></h2>
-	  <p>We appreciate your business, but that link no longer exists. Please head back to our <a href="<?php echo home_url('/');?>">homepage</a> to learn more about Legacy Hospitality.</p>
+  	<div class="col-md-12">
+      <img src="<?php echo get_template_directory_uri() . '/img/hanging-glasses.jpg'?>" alt="Error message" />
     </div>
 
-    <div class="col-md-6">
-      <img src="<?php echo get_template_directory_uri() . '/img/sorry-pencil.jpg'?>" alt="Error message" />
+    <div class="col-md-12">
+      <h2 class="bold">We truly <span class="red">apologize...</span></h2>
+	  <p>We appreciate your business, but that link no longer exists. Please head back to our <a href="<?php echo home_url('/');?>">homepage</a> to learn more about Legacy Hospitality.</p>
     </div>
   </div>
 </main>
