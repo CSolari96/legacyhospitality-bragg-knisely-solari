943f6351482fbabc56e782daf7d634124523413f
Valerie Bragg <vbragg@ufledu>
2019-11-10 11:04:55 -0500

    Add image ro 404 page

--- 404.php
+++ 404.php
@@ -1,10 +1,10 @@
 <?php get_header(); ?>
 <div class="container">
   <div class="row">
-    <main class="col-md-12">
+    <main class="col-md-12 cannot-find-page">
       <h2>We truly apoligize...</h2>
       <p>We appreciate your business, but that link no longer exists. Please head back to our <a href="<?php echo home_url('/');?>">homepage</a> to learn more about Legacy Hospitality.</p>
-      <img src="<?php get_template_directory_uri() . '/img/logo_placeholder.jpg'?>" alt="Error message" />
+      <img src="<?php get_template_directory_uri() . '/img/sorry-pencil.jpg'?>" alt="Error message" />
     </main>
 
   </div>
