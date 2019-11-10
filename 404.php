d347ed334ec2c3e42114ee5a526b9c608dc5e208
Valerie Bragg <vbragg@ufledu>
2019-11-10 11:20:01 -0500

    Add footer to 404 page

--- 404.php
+++ 404.php
@@ -2,10 +2,12 @@
 <div class="container">
   <div class="row">
     <main class="col-md-12 cannot-find-page">
-      <h2>We truly apoligize...</h2>
+      <h2 class= "bold">We truly apoligize...</h2>
       <p>We appreciate your business, but that link no longer exists. Please head back to our <a href="<?php echo home_url('/');?>">homepage</a> to learn more about Legacy Hospitality.</p>
-      <img src="<?php get_template_directory_uri() . '/img/sorry-pencil.jpg'?>" alt="Error message" />
+      <img src="<?php echo get_template_directory_uri() . '/img/sorry-pencil.jpg'?>" alt="Error message" />
     </main>
+
+  <?php get_footer(); ?>
 
   </div>
 </div>
