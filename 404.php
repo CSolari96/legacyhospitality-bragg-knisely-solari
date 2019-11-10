26d775062f4bbaffee5f5c5235d08eaf04fed7ad
Courtney Solari <csolari@ufl.edu>
2019-11-10 15:13:07 -0600

    Fix spelling error on 404 page

--- 404.php
+++ 404.php
@@ -3,7 +3,7 @@
   <div class="row">
 
     <div class="col-md-6">
-      <h2 class="bold">We truly apoligize...</h2>
+      <h2 class="bold">We truly <span class="red">apologize...</span></h2>
 	  <p>We appreciate your business, but that link no longer exists. Please head back to our <a href="<?php echo home_url('/');?>">homepage</a> to learn more about Legacy Hospitality.</p>
     </div>
 
