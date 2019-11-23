5c5e7128c1305bd014767653b4d920ea99d3c087
Valerie Bragg <vbragg@ufledu>
2019-11-23 16:26:27 -0500

    Add date to posts

--- single.php
+++ single.php
@@ -43,6 +43,10 @@
 
               <?php the_content(); ?>
               <p class="post-info"><?php echo "Published: " . get_the_date(); ?></p>
+              <p class="post-info"><?php echo "Published: " . get_the_date(); ?></p>
+              <p class="post-info"><?php echo "Published: " . get_the_date(); ?></p>
+              <p class="post-info"><?php echo "Published: " . get_the_date(); ?></p>
+
               <p class="post-info"><?php echo "Category: " . the_category(); ?></p>
 
         </main>
