cf58bb5f2f261f127800d14f07f667ae9c3a8045
Valerie Bragg <vbragg@ufledu>
2019-11-23 18:32:20 -0500

    Add padding and link color to archive page

--- archive.php
+++ archive.php
@@ -2,24 +2,30 @@
 
 get_header();
 ?>
-<div class="container-fluid">
-  <div class="row">
-    <div class="col-md-12">
-      <?php if(have_posts()){
-        while(have_posts()){
-          the_post(); ?>
-          <div class="col-md-12 individual-posts">
-            <h3 class="archive-title"><a href="<?php the_permalink() ?>"><?php the_title();?></a></h3>
-            <p>Published on: <?php the_time('F j, Y');?></p>
-            <p><?php the_excerpt(); ?></p>
-            <a href="<?php the_permalink() ?>">Read More</a>
-          </div>
-          <?php
-        }// ends while loop
-      } //ends if statement
-      ?>
+<div class="archive-page">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-12">
+        <?php if(have_posts()){
+          while(have_posts()){
+            the_post(); ?>
+            <div class="col-md-12 individual-posts">
+              <div class="padding-archive-posts">
+                <h3 class="archive-title"><a href="<?php the_permalink() ?>"><?php the_title();?></a></h3>
+                <p>Published on: <?php the_time('F j, Y');?></p>
+                <p><?php the_excerpt(); ?></p>
+                <div class="archive-read-more">
+                  <a href="<?php the_permalink() ?>">Read More</a>
+                </div>
+              </div>
+            </div>
+            <?php
+          }// ends while loop
+        } //ends if statement
+        ?>
 
-    </div>
+      </div>
+  </div>
 </div>
 
 <?php get_footer(); ?>
