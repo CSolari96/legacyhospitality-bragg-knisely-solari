f76c9ef7023f2cae80f3d7cb771597a29012b397
Valerie Bragg <vbragg@ufledu>
2019-11-23 17:02:31 -0500

    Add background color to archive posts

--- archive.php
+++ archive.php
@@ -8,8 +8,8 @@ get_header();
       <?php if(have_posts()){
         while(have_posts()){
           the_post(); ?>
-          <div class="col-md-12">
-            <h3><a href="<?php the_permalink() ?>"><?php the_title();?></a></h3>
+          <div class="col-md-12 individual-posts">
+            <h3><a href="<?php the_permalink() ?>" class="archive-title"><?php the_title();?></a></h3>
             <p>Published on: <?php the_time('F j, Y');?></p>
             <p><?php the_excerpt(); ?></p>
             <a href="<?php the_permalink() ?>">Read More</a>
--- single.php
+++ single.php
@@ -44,7 +44,6 @@
               <?php the_content(); ?>
               <p class="post-info"><?php echo "Published: " . get_the_date(); ?></p>
               <p>Category: <?php the_category(); ?></p> 
-              <p class="post-info"><?php echo "Category: " . the_category(); ?></p>
 
         </main>
       </div>
