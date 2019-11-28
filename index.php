5912786b160a8cae3c4c627b2e8bb25d99e44e4d
Valerie Bragg <vbragg@ufledu>
2019-11-28 11:29:03 -0500

    Put pagination on index.html

--- index.php
+++ index.php
@@ -26,6 +26,23 @@
         }//ends the if statement
        ?>
 
+       <div class="pagination-links">
+         <?php
+             global $wp_query;
+
+             $big = 999999999; // need an unlikely integer
+
+             echo paginate_links( array(
+             'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+             'format' => '?paged=%#%',
+             'current' => max( 1, get_query_var('paged') ),
+             'total' => $wp_query->max_num_pages
+              ) );
+
+              ?>
+
+       </div> <!--ends pagniation div--> 
+
     </main>
   </div>
 </div>
