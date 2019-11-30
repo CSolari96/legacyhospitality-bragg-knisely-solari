a8e6909a88e3154eb3918fe68d3888baf841fef1
Kenney Knisely <kknisely@mac.com>
2019-11-29 20:34:37 -0500

    Center button on homepage

--- page-home.php
+++ page-home.php
@@ -9,13 +9,15 @@ Template Post Type: page
 <div class="video-container">
 
 <?php get_header(); ?>
-    <main class="video-text">
+    <div class="video-text">
+
       <h2> Start your Legacy </h2>
+
       <div class="home-button">
-      <a href="<?php echo get_page_link( get_page_by_title( ABOUT )->ID ); ?>">Link Title</a>
+        <a href="<?php echo get_page_link( get_page_by_title( ABOUT )->ID ); ?>">Learn More</a>
       </div>
 
-    </main>
+    </div>
 </div>
 
 <?php get_footer(); ?>
