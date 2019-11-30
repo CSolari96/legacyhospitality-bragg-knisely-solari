10b572385fc95c9961ce837aca5e9cafac6b77bf
Kenney Knisely <kknisely@mac.com>
2019-11-29 20:08:16 -0500

    Add content to homepage

--- page-home.php
+++ page-home.php
@@ -10,6 +10,12 @@ Template Post Type: page
 
 <?php get_header(); ?>
     <main class="home-page">
+      
+      <?php if(have_posts()){
+        while(have_posts()){
+          the_post(); ?>
+
+          <?php the_content(); ?>
 
     </main>
 </div>
